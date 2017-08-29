c51f31e04ae87bf4ab742ccb78fbb89fb49c87a3
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2017-08-29 13:52:38 +0200

    Package installation

---
+++ config/bundles.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Symfony\Bundle\SecurityBundle\SecurityBundle' => ['all' => true],
+    'Symfony\Bundle\DebugBundle\DebugBundle' => ['dev' => true, 'test' => true],
+    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
+    'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
+    'Sensio\Bundle\DistributionBundle\SensioDistributionBundle' => ['all' => true],
+    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
+    'Knp\Bundle\PaginatorBundle\KnpPaginatorBundle' => ['all' => true],
+    'Knp\Bundle\MenuBundle\KnpMenuBundle' => ['all' => true],
+//    'Mopa\Bundle\BootstrapBundle\BootstrapBundle' => ['all' => true],
+    'Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle' => ['all' => true],
+];
