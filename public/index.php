cec79ba2543e741d4fd1c986a07fa392acf1522c
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2017-08-31 22:44:22 +0200

    Bootstrap the tests.

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV']) || in_array($_SERVER['APP_ENV'], ['dev', 'prod'])) {
+if (!isset($_SERVER['APP_ENV']) || in_array($_SERVER['APP_ENV'], ['dev'])) {
     (new Dotenv())->load(__DIR__.'/../.env');
 }
 
@@ -16,12 +16,9 @@ if ($_SERVER['APP_DEBUG'] ?? true) {
     // WARNING: You should setup permissions the proper way!
     // REMOVE the following PHP line and read
     // https://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
-    umask(0000);
 
     Debug::enable();
 }
-
-// Request::setTrustedProxies(['0.0.0.0/0'], Request::HEADER_FORWARDED);
 
 $kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', $_SERVER['APP_DEBUG'] ?? true);
 $request = Request::createFromGlobals();
---
+++ tests/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the IP-Trevise Application.
+ *
+ * PHP version 7.1
+ *
+ * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ *
+ * @category Entity
+ *
+ * @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ * @copyright 2017 Cerema — Alexandre Tranchant
+ * @license   Propriétaire Cerema
+ *
+ */
+use Symfony\Component\Dotenv\Dotenv;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$dotEnv = new Dotenv();
+$dotEnv->populate([
+    'APP_ENV' => 'test',
+    'DATABASE_URL' => 'postgres://postgres@127.0.0.1:5432/symfony?charset=utf8&application_name=iptrevise2'
+]);
+$dotEnv->load(__DIR__.'/../.env');
