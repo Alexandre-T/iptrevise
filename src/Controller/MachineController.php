e178aa33fc8c68980eaac7193fc49f736f4342aa
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2017-09-17 20:36:26 +0200

    Unlink an IP and a machine (from a machine)

--- src/Controller/MachineController.php
+++ src/Controller/MachineController.php
@@ -253,6 +253,62 @@ class MachineController extends Controller
     }
 
     /**
+     * Unlink an IP from its machine.
+     *
+     * @Route("/{id}/unlink", name="default_machine_unlink")
+     * @ParamConverter("ip", class="App:Ip")
+     * @Security("is_granted('ROLE_MANAGE_IP')")
+     *
+     * @param Request $request The request
+     * @param Ip      $ip      The IP entity
+     *
+     * @return RedirectResponse | Response
+     */
+    public function unlinkAction(Request $request, Ip $ip)
+    {
+        $trans = $this->get('translator.default');
+
+        if (null === $ip->getMachine()) {
+            //Flash Message
+            $session = $this->get('session');
+            $session->getFlashBag()->add('warning', $trans->trans('default.ip.unlink.error %ip%', [
+                '%ip%' => ip2long($ip->getIp()),
+            ]));
+
+            //Redirecting
+            return $this->redirectToRoute('default_machine_show', ['id' => $ip->getNetwork()->getId()]);
+        }
+
+        $form = $this->createUnlinkForm($ip);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //Prepare the message before unlinking.
+            $message = $trans->trans('default.ip.unlink %ip% %name%', [
+                '%ip%' => ip2long($ip->getIp()),
+                '%name%' => $ip->getMachine()->getLabel(),
+            ]);
+            $idMachine = $ip->getMachine()->getId();
+
+            //Unlinking
+            $ipManager = $this->get(IpManager::class);
+            $ipManager->unlink($ip);
+
+            //Flash Message
+            $session = $this->get('session');
+            $session->getFlashBag()->add('success', $message);
+
+            //Redirecting
+            return $this->redirectToRoute('default_machine_show', ['id' => $idMachine]);
+        } else {
+            return $this->render('@App/default/machine/unlink.html.twig', [
+                'confirm_form' => $form->createView(),
+                'ip' => $ip,
+            ]);
+        }
+    }
+
+    /**
      * Creates a form to delete a machine entity.
      *
      * @param Machine $machine The machine entity
@@ -288,6 +344,27 @@ class MachineController extends Controller
                 'attr' => ['class' => 'btn-danger confirm-delete'],
                 'icon' => 'trash-o',
                 'label' => 'form.ip.delete-ip.confirm',
+            ])
+            ->getForm()
+            ;
+    }
+
+    /**
+     * Creates a form to dissociate/unlink an ip from a machine.
+     *
+     * @param Ip $ip the Ip entity
+     *
+     * @return Form The form
+     */
+    private function createUnlinkForm(Ip $ip): Form
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('default_machine_unlink', array('id' => $ip->getId())))
+            ->setMethod('DELETE')
+            ->add('confirm', SubmitType::class, [
+                'attr' => ['class' => 'btn-danger confirm-delete'],
+                'icon' => 'hand-spock-o ',
+                'label' => 'form.ip.unlink.confirm',
             ])
             ->getForm()
             ;
