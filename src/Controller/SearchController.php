945c19c546428bcd6f1e612acd93c4aaf39a733e
Luca Sonet <lsonet001@enseirb-matmeca.fr>
2019-03-08 17:48:59 +0100

    Recherche

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * This file is part of the IP-Trevise Application.
- *
- * PHP version 7.1
- *
- * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
- *
- * @category Entity
- *
- * @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
- * @copyright 2017 Cerema
- * @license   CeCILL-B V1
- *
- * @see       http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
- */
+* This file is part of the IP-Trevise Application.
+*
+* PHP version 7.1
+*
+* (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+*
+* @category Entity
+*
+* @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
+* @copyright 2017 Cerema
+* @license   CeCILL-B V1
+*
+* @see       http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+*/
 
 namespace App\Controller;
 
@@ -41,72 +41,67 @@ use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 /**
- * Search CRUD Controller.
- *
- * @category Controller
- *
- * @author  Alexandre Tranchant <alexandre.tranchant@gmail.com>
- * @license CeCILL-B V1
- *
- * @Route("search")
- */
+* Search CRUD Controller.
+*
+* @category Controller
+*
+* @author  Alexandre Tranchant <alexandre.tranchant@gmail.com>
+* @license CeCILL-B V1
+*
+* @Route("search")
+*/
 class SearchController extends Controller
 {
-    /**
-     * Limit of machine per page for listing.
-     */
-    const LIMIT_PER_PAGE = 25;
+  /**
+  * Limit of machine per page for listing.
+  */
+  const LIMIT_PER_PAGE = 25;
 
 
 
 
-    /**
-     * Lists of all machines or ips whose matched
-     *
-     * @Route("/", name="default_search_index")
-     * @Method("GET")
-     * @Security("is_granted('ROLE_READ_MACHINE')")
-     *
-     * @param Request $request
-     *
-     * @return Response
-     */
+  /**
+  * Lists of all machines or ips whose matched
+  *
+  * @Route("/", name="default_search_index")
+  * @Method("GET")
+  * @Security("is_granted('ROLE_READ_MACHINE')")
+  *
+  * @param Request $request
+  *
+  * @return Response
+  */
 
 
-    public function indexAction(Request $request)
-    {
-      //Retrieving all services
-      $machineManager = $this->get(MachineManager::class);
-      $paginator = $this->get('knp_paginator');
-      $pagination = $paginator->paginate(
-          $machineManager->getQueryBuilder(), /* queryBuilder NOT result */
-          $request->query->getInt('page', 1)/*page number*/,
-          self::LIMIT_PER_PAGE,
-          ['defaultSortFieldName' => 'machine.label', 'defaultSortDirection' => 'asc']
-      );
+  public function indexAction(Request $request)
+  {
+    //Retrieving all services
+    $machineManager = $this->get(MachineManager::class);
+    $paginator = $this->get('knp_paginator');
+    $pagination = $paginator->paginate(
+      $machineManager->getQueryBuilder(), /* queryBuilder NOT result */
+      $request->query->getInt('page', 1)/*page number*/,
+      self::LIMIT_PER_PAGE,
+      ['defaultSortFieldName' => 'machine.label', 'defaultSortDirection' => 'asc']
+    );
 
 
-      $ipManager = $this->get(IpManager::class);
-      $pagination2 = $paginator->paginate(
-          $ipManager->getAll(), /* queryBuilder NOT result */
-          $request->query->getInt('page', 1)/*page number*/,
-          self::LIMIT_PER_PAGE,
-          ['defaultSortFieldName' => 'ip.ip', 'defaultSortDirection' => 'asc']
-      );
+    $ipManager = $this->get(IpManager::class);
+    $pagination2 = $paginator->paginate(
+      $ipManager->getAll(), /* queryBuilder NOT result */
+      $request->query->getInt('page', 1)/*page number*/,
+      self::LIMIT_PER_PAGE,
+      ['defaultSortFieldName' => 'ip.ip | ip', 'defaultSortDirection' => 'asc']
+    );
 
-      //$ip = $ipmanager->getAll();
+    if (isset($_GET['search'])) { $search = $_GET['search']; } else { $search = "FailToCatch"; }
 
-      if (isset($_GET['search'])) { $search = $_GET['search']; } else { $search = "FailToCatch"; }
+    return $this->render('@App/default/search/index.html.twig', [
+      'pagination' => $pagination,
+      'pagination2' => $pagination2,
+      'result' => $search
+    ]);
 
-
-
-
-        return $this->render('@App/default/search/index.html.twig', [
-          'pagination' => $pagination,
-          'pagination2' => $pagination2,
-          'result' => $search
-        ]);
-
-    }
   }
+}
 ?>
